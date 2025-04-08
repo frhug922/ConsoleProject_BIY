1766e41b52d99559d5058a92c97e8062017c500c
frhug922 <dlsrnjsl3@gmail.com>
2025-04-08 10:51:09 +0900

    [style] 클래스 내 지역변수명 수정
    변경
    GameMap Script
     - 지역변수명 앞에 언더바 추가

--- BabaIsYou/Map/GameMap.cs
+++ BabaIsYou/Map/GameMap.cs
@@ -7,37 +7,37 @@ using System.Threading.Tasks;
 namespace BabaIsYou {
     class GameMap {
         public Tile[,] Map { get; private set; }
-        private int width, height;
+        private int _width, _height;
 
         public GameMap(int level) {
             if (level == 1) {
-                width = 33;
-                height = 18;
+                _width = 33;
+                _height = 18;
             }
             else if (level == 2) {
-                width = 24;
-                height = 18;
+                _width = 24;
+                _height = 18;
             }
             else if (level == 3) {
-                width = 33; // TODO 레벨 별로 다른 맵 사이즈
-                height = 18;
+                _width = 33; // TODO 레벨 별로 다른 맵 사이즈
+                _height = 18;
             }
             else if (level == 4) {
-                width = 33;
-                height = 18;
+                _width = 33;
+                _height = 18;
             }
             else if (level == 5) {
-                width = 33;
-                height = 18;
+                _width = 33;
+                _height = 18;
             }
-            Map = new Tile[width, height];
+            Map = new Tile[_width, _height];
 
             InitializeMap();
         }
 
         private void InitializeMap() {
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
+            for (int y = 0; y < _height; y++) {
+                for (int x = 0; x < _width; x++) {
                     Map[x, y] = new EmptyTile(); // 기본적으로 빈 타일
                 }
             }
@@ -50,8 +50,8 @@ namespace BabaIsYou {
         }
 
         public void PrintMap() {
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
+            for (int y = 0; y < _height; y++) {
+                for (int x = 0; x < _width; x++) {
                     Console.Write(GetTileSymbol(Map[x, y]) + " ");
                 }
                 Console.WriteLine();
